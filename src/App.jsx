e87d8e2c2f87eaa35637beba1bb93be29c16e476
Timotej <timotej903@gmail.com>
2025-02-12 16:21:28 +0100

    Added header and style it. Added searchbar background

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SearchbarComponent from './components/Searchbar'
 function App() {
   return (
     <div>
+      <div className="header"><h2>ravenous</h2></div>
       <SearchbarComponent />
       <BusinessList />
     </div>
