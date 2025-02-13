6971c8ba35ffaadd7593dfec781e0c7687488fa3
Timotej <timotej903@gmail.com>
2025-02-13 10:51:45 +0100

    Adding and styling filters

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,6 +3,17 @@ import React from "react";
 function SearchbarComponent() {
     return(
         <div className="searchbar">
+            <div className="filter">
+                <div className="best-mach">
+                    <h2>Best<br />Mach</h2>
+                </div>
+                <div className="higest-rated">
+                    <h2>Highest<br />Rated</h2>
+                </div>
+                <div className="most-reviewed">
+                    <h2>Most<br />Reviewed</h2>
+                </div>
+            </div>
             <div className="inputs">
                 <input type="text" placeholder="Seach Businesses"/>
                 <input type="text" placeholder="Where?"/> 
